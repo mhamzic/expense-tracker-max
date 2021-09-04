0086601986fa3ae13adfa49f6bb0355e76c1c3b7
mhamzic <mhamzic@gmail.com>
2021-09-04 19:30:01 +0200

    finished form hiding/showing on button input

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+  // const [showButton, setShowButton] = useState(true);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,11 +13,24 @@ const NewExpense = (props) => {
     };
 
     props.onAddExpense(expenseData);
+    setShowForm(false);
+    // setShowButton(true);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+    // setShowButton(false);
+  };
+
+  const hideFormHandler = () => {
+    setShowForm(false);
+    // setShowButton(true);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showForm && <button onClick={showFormHandler}>Add New Expenses</button>}
+      {showForm && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} hideForm={hideFormHandler} />}
     </div>
   );
 };
